c02f5eed58274fc4ef3d4ab40f010786088ab53b
Stefan Wolfrum <metawops@gmail.com>
2022-05-29 23:03:51 +0200

    Implemented core functionality

--- main.ts
+++ main.ts
@@ -2,136 +2,229 @@ import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Set
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface Table2CSVSettings {
+   exportPath: string;
+   fileNumber: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: Table2CSVSettings = {
+   exportPath: '',
+   fileNumber: '001'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class Table2CSVPlugin extends Plugin {
+   settings: Table2CSVSettings;
 
-	async onload() {
-		await this.loadSettings();
+   async onload() {
+      console.log("In onload().");
+      await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+      this.addCommand({
+         id: 'table-to-csv-export',
+         name: 'Export all tables to CSV files',
+         checkCallback: (checking: boolean) => {
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+            const view = this.app.workspace.getActiveViewOfType(MarkdownView);
+            
+            // const markdownView = this.app.workspace.activeLeaf.view as MarkdownView;
+            // const livePreviewActive: boolean = markdownView.getState().field(editorLivePreviewField);
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+            if (view) {
+               if (!checking) {
+                  // Here we can actually start with our work
+                  console.log("table-to-csv-export command triggered.")
+                  const viewMode = view.getMode();
+                  console.log("viewMode =", viewMode);
+                  if (viewMode=="preview") {
+                     console.log("We're in reading mode and can now work on the HTML tables! :-)");
+                     console.log("Here's the HTML of the active pane in reading mode:");
+                     console.log(view.previewMode.containerEl);
+                     
+                     // Now convert the tables
+                     const csvString = htmlToCSV(view.previewMode.containerEl);
+                     console.log("And here's the HTML tables converted to CSV:");
+                     console.log(csvString);
+                     
+                     this.app.vault.create(`./csv-file-${this.settings.fileNumber}.csv`, csvString)
+   
+                        .catch( (error) => {
+                           console.log(error.message);
+                           const errorMessage = `Error: ${error.message}`;
+                           new Notice(errorMessage);
+                        })
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+                        .finally( () => {
+                           let fn: number = +this.settings.fileNumber;
+                           fn++;
+                           let newFileNumberString: string = fn + "";
+                           while (newFileNumberString.length < 3) newFileNumberString = "0" + newFileNumberString;
+                           this.settings.fileNumber = newFileNumberString;
+                        })
+                        
+                     
+                  }
+                  else {
+                     new Notice('This command only works on panes in reading mode! – No CSV files were written.');
+                  }
+               }
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+               return true;
+            }
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+            return false;
+         }
+      });
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
 
-	onunload() {
+      // This creates an icon in the left ribbon.
+      const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
+         // Called when the user clicks the icon.
+         new Notice('This is a notice!');
+      });
+      // Perform additional things with the ribbon
+      ribbonIconEl.addClass('my-plugin-ribbon-class');
 
-	}
+      // This adds a status bar item to the bottom of the app. Does not work on mobile apps.
+      // const statusBarItemEl = this.addStatusBarItem();
+      // statusBarItemEl.setText('Status Bar Text');
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+      // This adds a simple command that can be triggered anywhere
+      // this.addCommand({
+      //    id: 'open-sample-modal-simple',
+      //    name: 'Open sample modal (simple)',
+      //    callback: () => {
+      //       new SampleModal(this.app).open();
+      //    }
+      // });
+      // This adds an editor command that can perform some operation on the current editor instance
+      // this.addCommand({
+      //    id: 'sample-editor-command',
+      //    name: 'Sample editor command',
+      //    editorCallback: (editor: Editor, view: MarkdownView) => {
+      //       console.log(editor.getSelection());
+      //       editor.replaceSelection('Sample Editor Command');
+      //    }
+      // });
+      // This adds a complex command that can check whether the current state of the app allows execution of the command
+      // this.addCommand({
+      //    id: 'open-sample-modal-complex',
+      //    name: 'Open sample modal (complex)',
+      //    checkCallback: (checking: boolean) => {
+      //       // Conditions to check
+      //       const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
+      //       if (markdownView) {
+      //          // If checking is true, we're simply "checking" if the command can be run.
+      //          // If checking is false, then we want to actually perform the operation.
+      //          if (!checking) {
+      //             new SampleModal(this.app).open();
+      //          }
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+      //          // This command will only show up in Command Palette when the check function returns true
+      //          return true;
+      //       }
+      //    }
+      // });
+
+      // This adds a settings tab so the user can configure various aspects of the plugin
+      this.addSettingTab(new Table2CSVSettingTab(this.app, this));
+
+      // If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
+      // Using this function will automatically remove the event listener when this plugin is disabled.
+      // this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
+      //    console.log('click', evt);
+      // });
+
+      // When registering intervals, this function will automatically clear the interval when the plugin is disabled.
+      //this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+   }
+
+    onunload() {
+       console.log("In onunload().");
+    }
+
+    async loadSettings() {
+       this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+    }
+
+    async saveSettings() {
+       await this.saveData(this.settings);
+    }
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+
+function htmlToCSV(html: HTMLElement) {
+	var data = [];
+	var rows = html.querySelectorAll("table tr");
+			
+	for (var i = 0; i < rows.length; i++) {
+		var row = [], cols = rows[i].querySelectorAll("td, th");
+				
+		for (var j = 0; j < cols.length; j++) {
+		        row.push(cols[j].innerText);
+        }
+		        
+		data.push(row.join(",")); 		
 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+   return data.join("\n");
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+// class SampleModal extends Modal {
+//     constructor(app: App) {
+//        super(app);
+//     }
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+//    onOpen() {
+//       const {contentEl} = this;
+//       contentEl.setText('Woah!');
+//    }
 
-	display(): void {
-		const {containerEl} = this;
+//    onClose() {
+//       const {contentEl} = this;
+//       contentEl.empty();
+//    }
+// }
 
-		containerEl.empty();
+class Table2CSVSettingTab extends PluginSettingTab {
+   plugin: Table2CSVPlugin;
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+   constructor(app: App, plugin: Table2CSVPlugin) {
+      super(app, plugin);
+      this.plugin = plugin;
+   }
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+   display(): void {
+      const {containerEl} = this;
+
+      containerEl.empty();
+
+      containerEl.createEl('h2', {text: 'Settings for the Table to CSV Plugin.'});
+
+      new Setting(containerEl)
+         .setName('CSV file export path')
+         .setDesc('Enter the path where the exported CSV file should be saved. If no path is set the CSV file will be saved into your vault folder.')
+         .addText(text => text
+            .setPlaceholder('<enter a path>')
+            .setValue(this.plugin.settings.exportPath)
+            .onChange(async (value) => {
+               console.log('path: ' + value);
+               this.plugin.settings.exportPath = value;
+               await this.plugin.saveSettings();
+            }));
+
+      new Setting(containerEl)
+         .setName('File Number addendum')
+         .setDesc('This number gets added to the base filename and incremented after each export.')
+         .addText(text => text
+            .setPlaceholder('')
+            .setValue(this.plugin.settings.fileNumber)
+            .onChange(async (value) => {
+               console.log('fileNumber: ' + value);
+               this.plugin.settings.fileNumber = value;
+               await this.plugin.saveSettings();
+            }));
+      
+
+
+   }
 }
